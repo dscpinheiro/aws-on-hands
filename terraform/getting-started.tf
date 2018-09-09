6d1f93cce6fbf45af8d1f7356382a230612164ca
Daniel Schmidt Pinheiro <dscpinheiro@gmail.com>
2018-09-09 17:07:59 -0400

    Added elastic IP resource

--- terraform/getting-started.tf
+++ terraform/getting-started.tf
@@ -2,7 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "sample_instance" {
   ami = "ami-b374d5a5"
   instance_type = "t2.micro"
+}
+
+resource "aws_eip" "elastic_ip" {
+  instance = "${aws_instance.sample_instance.id}"
 }
