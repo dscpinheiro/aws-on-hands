9f3693edc12e5fa7f3a3bea659f7ae438cd2dd69
Daniel Schmidt Pinheiro <dscpinheiro@gmail.com>
2018-09-09 17:36:18 -0400

    Added variables and outputs to template

--- terraform/getting-started.tf
+++ terraform/getting-started.tf
@@ -1,12 +1,16 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "${var.region}"
 }
 
 resource "aws_instance" "sample_instance" {
-  ami = "ami-b374d5a5"
+  ami = "${lookup(var.amis, var.region)}"
   instance_type = "t2.micro"
 }
 
 resource "aws_eip" "elastic_ip" {
   instance = "${aws_instance.sample_instance.id}"
+}
+
+output "ip" {
+  value = "${aws_eip.elastic_ip.public_ip}"
 }
