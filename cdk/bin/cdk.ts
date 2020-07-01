4a4c8faa2ed26476209ddbe138f58585f7a14ff6
Daniel Schmidt Pinheiro <3842788+dscpinheiro@users.noreply.github.com>
2020-07-01 15:02:38 -0400

    Add fanout example

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
+
 import { VpcStack } from '../lib/vpc-stack';
+import { FanoutStack } from '../lib/fanout-stack';
 
 const app = new cdk.App();
 new VpcStack(app, 'VpcStack');
+new FanoutStack(app, 'FanoutStack');
